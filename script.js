5a4df8d299b91eedc583b98468fc9b4d70215e2d
Myke <programacaoweb5@gmail.com>
2021-08-25 17:47:04 -0300

    64 - Entrega: SnapCrackle

---
+++ script.js
@@ -0,0 +1,21 @@
+function snapCrackle(maxValue) {
+let lista = []
+    for (let i = 1; i <= maxValue; i++) {
+        if(i % 2 === 1){
+            lista.push('Snap')
+        }else if(i % 5 === 0){
+            lista.push('Crackle')
+        }
+        if(i % 2 === 1 && i % 5 === 0){
+            lista.push('SnapCrackle')
+        }
+        if(!(i % 2 === 1 && i % 5 === 0)){
+            lista.push(i)
+        } 
+     
+    }     
+    return lista.join(', ')
+}
+
+console.log(snapCrackle(15))
+
